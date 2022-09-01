459729e1a78e1f47660dd1366c3201fd800f9430
liuliang16 <liuliang16@xiaomi.com>
2022-09-01 16:33:15 +0800

    add uicustomview

---
+++ chapter3/UICustomViews/app/src/main/java/com/example/uicustomviews/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.uicustomviews;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        ActionBar supportActionBar = getSupportActionBar();
+//        if(supportActionBar != null) {
+//            supportActionBar.hide();
+//        }
+    }
+}
